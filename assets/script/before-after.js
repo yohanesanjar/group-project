5e82a8d9cec97484379f6dcb0788beadab1fb572
yohanesanjar <yohanesanjar@gmail.com>
2022-10-23 15:38:23 +0700

    Final group-project

--- assets/script/before-after.js
+++ assets/script/before-after.js
@@ -1,3 +1,7 @@
+function beforeSign(){
+    alert('anda belum sign in')
+    window.location = '../../signin.html'
+}
 $(document).ready(function () {
     let x = localStorage.getItem("token")
     let video = document.getElementById('video')
@@ -8,12 +12,11 @@ $(document).ready(function () {
     if (x) {
         document.getElementById('avatar').style.display = 'block'
         document.getElementById('masuk').style.display = 'none'
-        document.getElementById('videoDisabled').style.display = 'none'
-
+        document.getElementById('video-before').style.display = 'none'
+        
         video.appendChild(videoEl)
     } else {
         document.getElementById('avatar').style.display = 'none'
         document.getElementById('masuk').style.display = 'block'
     }
 });
-
